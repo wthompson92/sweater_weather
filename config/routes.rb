c2cfbc959267206054a1f3042d9b74eda392faf5
username <wnt925@gmail.com>
2019-09-16 14:11:45 -0600

    Add service for unsplash

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecasts#show'
       get '/gifs', to: 'gifs#index'
+      get '/gifs', to: 'gifs#index'
     end
   end
 end
