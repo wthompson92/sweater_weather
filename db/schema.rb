28197fd7e9d031ea7806fd194acd8879cbaa02df
username <wnt925@gmail.com>
2019-09-16 18:39:08 -0600

    Switch DB from Sql to PG

--- db/schema.rb
+++ db/schema.rb
@@ -12,19 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2019_09_16_205411) do
 
-  create_table "forecasts", force: :cascade do |t|
-    t.string "temp"
-    t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.string "name"
-    t.float "coords"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email"
